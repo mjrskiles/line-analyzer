f6084fb95c36d3788cedd3c9a6fe9c7616b0d09d
Michael Skiles <mjrskiles@gmail.com>
2021-09-23 22:09:18 -0500

    tft testing

---
+++ cmake-build-teensy41/CMakeFiles/Z_DUMMY_TARGET.dir/DependInfo.cmake
@@ -0,0 +1,135 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/michael/projects/mc-audio/line-analyzer/src/main.cpp" "/Users/michael/projects/mc-audio/line-analyzer/cmake-build-teensy41/CMakeFiles/Z_DUMMY_TARGET.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../src"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/ILI9341_t3"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/SPI"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/cores/teensy4"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/ADC"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/AccelStepper/src"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/Adafruit_NeoPixel"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/Adafruit_STMPE610"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/Adafruit_VS1053"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/Adafruit_nRF8001"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/Adafruit_nRF8001/utility"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/AltSoftSerial"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/Artnet"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/Audio"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/Audio/utility"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/Bounce"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/Bounce2/src"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/CapacitiveSensor"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/CryptoAccel/src"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/DS1307RTC"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/DmxSimple"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/DogLcd"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/EEPROM"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/EasyTransfer"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/EasyTransferI2C"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/Encoder"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/Encoder/utility"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/Entropy"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/Ethernet/src"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/FNET/src"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/FastCRC"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/FastLED"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/FlexCAN"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/FlexCAN_T4"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/FlexIO_t4/src"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/FlexiTimer2"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/FreqCount"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/FreqMeasure"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/FreqMeasureMulti"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/FrequencyTimer2"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/ILI9488_t3/src"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/IRremote"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/Keypad/src"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/LedControl/src"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/LedDisplay"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/LiquidCrystal/src"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/LiquidCrystalFast"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/LittleFS/src"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/LowPower"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/MFRC522/src"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/MIDI/src"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/Metro"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/MsTimer2"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/NXPMotionSense"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/NXPMotionSense/utility"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/NativeEthernet/src"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/OSC"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/OctoWS2811"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/OneWire"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/PS2Keyboard"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/PS2Keyboard/utility"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/PWMServo"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/Ping"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/PulsePosition"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/QuadEncoder"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/RA8875"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/RadioHead"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/ResponsiveAnalogRead/src"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/SD/src"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/SPIFlash"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/ST7735_t3"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/SdFat/src"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/SerialFlash"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/Servo"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/ShiftPWM"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/Snooze/src"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/SoftPWM"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/SoftwareSerial"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/TFT_ILI9163C"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/Talkie"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/TeensyThreads"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/Time"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/TimeAlarms"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/TimerOne"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/TimerThree"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/TinyGPS"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/Tlc5940"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/TouchScreen"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/USBHost_t36"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/USBHost_t36/utility"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/UTFT"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/VirtualWire"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/WS2812Serial"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/Wire"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/Wire/utility"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/XBee"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/XPT2046_Touchscreen"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/i2c_t3"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/ks0108"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/ssd1351"
+  "/Users/michael/.platformio/packages/framework-arduinoteensy/libraries/x10"
+  "/Users/michael/.platformio/packages/toolchain-gccarmnoneeabi/arm-none-eabi/include/c++/5.4.1"
+  "/Users/michael/.platformio/packages/toolchain-gccarmnoneeabi/arm-none-eabi/include/c++/5.4.1/arm-none-eabi"
+  "/Users/michael/.platformio/packages/toolchain-gccarmnoneeabi/lib/gcc/arm-none-eabi/5.4.1/include"
+  "/Users/michael/.platformio/packages/toolchain-gccarmnoneeabi/lib/gcc/arm-none-eabi/5.4.1/include-fixed"
+  "/Users/michael/.platformio/packages/toolchain-gccarmnoneeabi/arm-none-eabi/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,125 @@
 //
 #include <Arduino.h>
 #include <ILI9341_t3.h>
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
 
-ILI9341_t3 tft;
+
+#define TFT_DC 36
+#define TFT_CS 10
+#define DISPLAY_X_SIZE 320
+#define DISPLAY_Y_SIZE 240
+
+
+// GUItool: begin automatically generated code
+AudioInputI2S            i2s1;           //xy=139,91
+AudioMixer4              mixer1;         //xy=312,134
+AudioOutputI2S           i2s2;           //xy=392,32
+AudioAnalyzeFFT1024      fft1024;        //xy=467,147
+AudioConnection          patchCord1(i2s1, 0, mixer1, 0);
+AudioConnection          patchCord2(i2s1, 0, i2s2, 0);
+AudioConnection          patchCord3(i2s1, 1, mixer1, 1);
+AudioConnection          patchCord4(i2s1, 1, i2s2, 1);
+AudioConnection          patchCord5(mixer1, fft1024);
+AudioControlSGTL5000     audioShield;    //xy=366,225
+// GUItool: end automatically generated code
+
+
+const int myInput = AUDIO_INPUT_LINEIN;
+//const int myInput = AUDIO_INPUT_MIC;
+
+
+// The scale sets how much sound is needed in each frequency range to
+// show all 8 bars.  Higher numbers are more sensitive.
+float scale = 60.0;
+
+// An array to hold the 16 frequency bands
+float level[16];
+
+// This array holds the on-screen levels.  When the signal drops quickly,
+// these are used to lower the on-screen level 1 bar per update, which
+// looks more pleasing to corresponds to human sound perception.
+int   shown[16];
+
+const int barPositionsX[16] = {0, 20, 40, 60,
+                         80, 100, 120, 140,
+                         160, 180, 200, 220,
+                         240, 260, 280, 300};
+const int barMaxHeight = 160;
+
+
+ILI9341_t3 tft = ILI9341_t3(TFT_CS, TFT_DC);
 
 void setup() {
+    AudioMemory(24);
+    audioShield.enable();
+    audioShield.inputSelect(myInput);
+    audioShield.volume(0.5);
+    mixer1.gain(0, 0.5);
+    mixer1.gain(1, 0.5);
+
     tft.begin();
+    tft.setRotation(1);
 }
 
 void loop() {
+    if (fft1024.available()) {
+        // read the 512 FFT frequencies into 16 levels
+        // music is heard in octaves, but the FFT data
+        // is linear, so for the higher octaves, read
+        // many FFT bins together.
+        level[0] =  fft1024.read(0);
+        level[1] =  fft1024.read(1);
+        level[2] =  fft1024.read(2, 3);
+        level[3] =  fft1024.read(4, 6);
+        level[4] =  fft1024.read(7, 10);
+        level[5] =  fft1024.read(11, 15);
+        level[6] =  fft1024.read(16, 22);
+        level[7] =  fft1024.read(23, 32);
+        level[8] =  fft1024.read(33, 46);
+        level[9] =  fft1024.read(47, 66);
+        level[10] = fft1024.read(67, 93);
+        level[11] = fft1024.read(94, 131);
+        level[12] = fft1024.read(132, 184);
+        level[13] = fft1024.read(185, 257);
+        level[14] = fft1024.read(258, 359);
+        level[15] = fft1024.read(360, 511);
+        // See this conversation to change this to more or less than 16 log-scaled bands?
+        // https://forum.pjrc.com/threads/32677-Is-there-a-logarithmic-function-for-FFT-bin-selection-for-any-given-of-bands
 
+
+
+        for (int i=0; i<16; i++) {
+            tft.setCursor(barPositionsX[i], 230);
+            tft.print(i);
+            Serial.print(level[i]);
+
+            // TODO: conversion from FFT data to display bars should be
+            // exponentially scaled.  But how keep it a simple example?
+            int val = level[i] * scale;
+            if (val > 8) val = 8;
+
+            if (val >= shown[i]) {
+                shown[i] = val;
+            } else {
+                if (shown[i] > 0) shown[i] = shown[i] - 1;
+                val = shown[i];
+            }
+
+            //Serial.print(shown[i]);
+            Serial.print(" ");
+
+//            // print each custom digit
+//            if (shown[i] == 0) {
+//
+//            } else {
+//                lcd.write(shown[i] - 1);
+//            }
+
+            tft.drawRect(barPositionsX[i], 0, 20, 12 +(shown[i] * 20), ILI9341_GREEN);
+        }
+        Serial.print(" cpu:");
+        Serial.println(AudioProcessorUsageMax());
+    }
 }
